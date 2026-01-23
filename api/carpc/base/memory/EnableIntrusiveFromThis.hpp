84f52b88a604cf987e43a3e2f74c4d662a164f31
Dmitriy Terletskiy <daterletskiy@gmail.com>
2026-01-23 18:04:55 +0200

    refactoring: IntrusivePtr and EnableIntrusiveFromThis
    Move implementation outside of the class.
    
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- api/carpc/base/memory/EnableIntrusiveFromThis.hpp
+++ api/carpc/base/memory/EnableIntrusiveFromThis.hpp
@@ -22,29 +22,34 @@ namespace carpc::memory {
          ~EnableIntrusiveFromThis( ) = default;
 
       public:
-         IntrusivePtr< T > intrusive_from_this( )
-         {
-#ifndef NO_DEBUG
-            assert(
-                  static_cast< const T* >( this )->ref_count( ) > 0 &&
-                  "intrusive_from_this() called on unmanaged object"
-               );
-#endif
-            return IntrusivePtr< T >( static_cast< T* >( this ) );
-         }
-
-         IntrusivePtr< const T > intrusive_from_this( ) const
-         {
-#ifndef NO_DEBUG
-            assert(
-                  static_cast< const T* >( this )->ref_count( ) > 0 &&
-                  "intrusive_from_this() called on unmanaged object"
-               );
-#endif
-            return IntrusivePtr< const T >(
-                  static_cast< const T* >( this )
-               );
-         }
+         IntrusivePtr< T > intrusive_from_this( );
+         IntrusivePtr< const T > intrusive_from_this( ) const;
    };
+
+
+
+   template< typename T >
+   IntrusivePtr< T > EnableIntrusiveFromThis< T >::intrusive_from_this( )
+   {
+#ifndef NO_DEBUG
+      assert(
+            static_cast< const T* >( this )->ref_count( ) > 0 &&
+            "intrusive_from_this() called on unmanaged object"
+         );
+#endif
+      return IntrusivePtr< T >( static_cast< T* >( this ) );
+   }
+
+   template< typename T >
+   IntrusivePtr< const T > EnableIntrusiveFromThis< T >::intrusive_from_this( ) const
+   {
+#ifndef NO_DEBUG
+      assert(
+            static_cast< const T* >( this )->ref_count( ) > 0 &&
+            "intrusive_from_this() called on unmanaged object"
+         );
+#endif
+      return IntrusivePtr< const T >( static_cast< const T* >( this ) );
+   }
 
 } // namespace carpc::memory
--- api/carpc/base/memory/IntrusivePtr.hpp
+++ api/carpc/base/memory/IntrusivePtr.hpp
@@ -23,184 +23,263 @@ namespace carpc::memory {
          using element_type = T;
 
       public:
-         IntrusivePtr( ) noexcept
-            : m_ptr( nullptr )
-         {
-         }
+         IntrusivePtr( ) noexcept;
 
-         explicit IntrusivePtr( T* ptr )
-            : m_ptr( ptr )
-         {
-            if( nullptr != m_ptr )
-            {
-               m_ptr->add_ref( );
-            }
-         }
+         explicit IntrusivePtr( T* ptr );
 
-         IntrusivePtr( T* ptr, AdoptTag ) noexcept
-            : m_ptr( ptr )
-         {
-         }
+         IntrusivePtr( T* ptr, AdoptTag ) noexcept;
 
-         IntrusivePtr( const IntrusivePtr& other )
-            : m_ptr( other.m_ptr )
-         {
-            if( nullptr != m_ptr )
-            {
-               m_ptr->add_ref( );
-            }
-         }
+         IntrusivePtr( const IntrusivePtr& other );
 
          template< typename U >
-         IntrusivePtr( const IntrusivePtr< U >& other )
             requires std::is_convertible_v< U*, T* >
-            : m_ptr( other.get( ) )
-         {
-            if( nullptr != m_ptr )
-            {
-               m_ptr->add_ref( );
-            }
-         }
+         IntrusivePtr( const IntrusivePtr< U >& other );
 
-         IntrusivePtr( IntrusivePtr&& other ) noexcept
-            : m_ptr( other.m_ptr )
-         {
-            other.m_ptr = nullptr;
-         }
+         IntrusivePtr( IntrusivePtr&& other ) noexcept;
 
          template< typename U >
-         IntrusivePtr( IntrusivePtr< U >&& other ) noexcept
             requires std::is_convertible_v< U*, T* >
-            : m_ptr( other.get( ) )
-         {
-            other.reset( );
-         }
+         IntrusivePtr( IntrusivePtr< U >&& other ) noexcept;
 
-         ~IntrusivePtr( )
-         {
-            if( nullptr != m_ptr )
-            {
-               m_ptr->release( );
-            }
-         }
+         ~IntrusivePtr( );
 
       public:
-         IntrusivePtr& operator=( const IntrusivePtr& other )
-         {
-            if( this != &other )
-            {
-               reset( other.m_ptr );
-            }
-            return *this;
-         }
+         IntrusivePtr& operator=( const IntrusivePtr& other );
 
          template< typename U >
-         IntrusivePtr& operator=( const IntrusivePtr< U >& other )
             requires std::is_convertible_v< U*, T* >
-         {
-            reset( other.get( ) );
-            return *this;
-         }
+         IntrusivePtr& operator=( const IntrusivePtr< U >& other );
 
-         IntrusivePtr& operator=( IntrusivePtr&& other ) noexcept
-         {
-            if( this != &other )
-            {
-               reset( );
-               m_ptr = other.m_ptr;
-               other.m_ptr = nullptr;
-            }
-            return *this;
-         }
+         IntrusivePtr& operator=( IntrusivePtr&& other ) noexcept;
 
          template< typename U >
-         IntrusivePtr& operator=( IntrusivePtr< U >&& other ) noexcept
             requires std::is_convertible_v< U*, T* >
-         {
-            reset( );
-            m_ptr = other.get( );
-            other.reset( );
-            return *this;
-         }
+         IntrusivePtr& operator=( IntrusivePtr< U >&& other ) noexcept;
 
       public:
-         void reset( T* ptr = nullptr )
-         {
-            if( ptr == m_ptr )
-            {
-               return;
-            }
+         void reset( T* ptr = nullptr );
 
-            if( nullptr != m_ptr )
-            {
-               m_ptr->release( );
-            }
+         T* release( ) noexcept;
 
-            m_ptr = ptr;
-
-            if( nullptr != m_ptr )
-            {
-               m_ptr->add_ref( );
-            }
-         }
-
-         T* release( ) noexcept
-         {
-            T* tmp = m_ptr;
-            m_ptr = nullptr;
-            return tmp;
-         }
-
-         void swap( IntrusivePtr& other ) noexcept
-         {
-            std::swap( m_ptr, other.m_ptr );
-         }
+         void swap( IntrusivePtr& other ) noexcept;
 
       public:
-         T* get( ) const noexcept
-         {
-            return m_ptr;
-         }
+         T* get( ) const noexcept;
 
-         T& operator*( ) const
-         {
-            return *m_ptr;
-         }
+         T& operator*( ) const;
 
-         T* operator->( ) const noexcept
-         {
-            return m_ptr;
-         }
+         T* operator->( ) const noexcept;
 
-         explicit operator bool( ) const noexcept
-         {
-            return nullptr != m_ptr;
-         }
+         explicit operator bool( ) const noexcept;
 
       public:
-         bool operator==( const IntrusivePtr& other ) const noexcept
-         {
-            return m_ptr == other.m_ptr;
-         }
+         bool operator==( const IntrusivePtr& other ) const noexcept;
 
-         bool operator!=( const IntrusivePtr& other ) const noexcept
-         {
-            return m_ptr != other.m_ptr;
-         }
+         bool operator!=( const IntrusivePtr& other ) const noexcept;
 
-         bool operator==( std::nullptr_t ) const noexcept
-         {
-            return nullptr == m_ptr;
-         }
+         bool operator==( std::nullptr_t ) const noexcept;
 
-         bool operator!=( std::nullptr_t ) const noexcept
-         {
-            return nullptr != m_ptr;
-         }
+         bool operator!=( std::nullptr_t ) const noexcept;
 
       private:
          T* m_ptr;
    };
+
+
+
+   template< typename T >
+   IntrusivePtr< T >::IntrusivePtr( ) noexcept
+      : m_ptr( nullptr )
+   {
+   }
+
+   template< typename T >
+   IntrusivePtr< T >::IntrusivePtr( T* ptr )
+      : m_ptr( ptr )
+   {
+      if( nullptr != m_ptr )
+      {
+         m_ptr->add_ref( );
+      }
+   }
+
+   template< typename T >
+   IntrusivePtr< T >::IntrusivePtr( T* ptr, AdoptTag ) noexcept
+      : m_ptr( ptr )
+   {
+   }
+
+   template< typename T >
+   IntrusivePtr< T >::IntrusivePtr( const IntrusivePtr& other )
+      : m_ptr( other.m_ptr )
+   {
+      if( nullptr != m_ptr )
+      {
+         m_ptr->add_ref( );
+      }
+   }
+
+   template< typename T >
+   template< typename U >
+      requires std::is_convertible_v< U*, T* >
+   IntrusivePtr< T >::IntrusivePtr( const IntrusivePtr< U >& other )
+      : m_ptr( other.get( ) )
+   {
+      if( nullptr != m_ptr )
+      {
+         m_ptr->add_ref( );
+      }
+   }
+
+   template< typename T >
+   IntrusivePtr< T >::IntrusivePtr( IntrusivePtr&& other ) noexcept
+      : m_ptr( other.m_ptr )
+   {
+      other.m_ptr = nullptr;
+   }
+
+   template< typename T >
+   template< typename U >
+      requires std::is_convertible_v< U*, T* >
+   IntrusivePtr< T >::IntrusivePtr( IntrusivePtr< U >&& other ) noexcept
+      : m_ptr( other.get( ) )
+   {
+      other.reset( );
+   }
+
+   template< typename T >
+   IntrusivePtr< T >::~IntrusivePtr( )
+   {
+      if( nullptr != m_ptr )
+      {
+         m_ptr->release( );
+      }
+   }
+
+   template< typename T >
+   IntrusivePtr< T >& IntrusivePtr< T >::operator=( const IntrusivePtr& other )
+   {
+      if( this != &other )
+      {
+         reset( other.m_ptr );
+      }
+      return *this;
+   }
+
+   template< typename T >
+   template< typename U >
+      requires std::is_convertible_v< U*, T* >
+   IntrusivePtr< T >& IntrusivePtr< T >::operator=( const IntrusivePtr< U >& other )
+   {
+      reset( other.get( ) );
+      return *this;
+   }
+
+   template< typename T >
+   IntrusivePtr< T >& IntrusivePtr< T >::operator=( IntrusivePtr&& other ) noexcept
+   {
+      if( this != &other )
+      {
+         reset( );
+         m_ptr = other.m_ptr;
+         other.m_ptr = nullptr;
+      }
+      return *this;
+   }
+
+   template< typename T >
+   template< typename U >
+      requires std::is_convertible_v< U*, T* >
+   IntrusivePtr< T >& IntrusivePtr< T >::operator=( IntrusivePtr< U >&& other ) noexcept
+   {
+      reset( );
+      m_ptr = other.get( );
+      other.reset( );
+      return *this;
+   }
+
+   template< typename T >
+   void IntrusivePtr< T >::reset( T* ptr )
+   {
+      if( ptr == m_ptr )
+      {
+         return;
+      }
+
+      if( nullptr != m_ptr )
+      {
+         m_ptr->release( );
+      }
+
+      m_ptr = ptr;
+
+      if( nullptr != m_ptr )
+      {
+         m_ptr->add_ref( );
+      }
+   }
+
+   template< typename T >
+   T* IntrusivePtr< T >::release( ) noexcept
+   {
+      T* tmp = m_ptr;
+      m_ptr = nullptr;
+      return tmp;
+   }
+
+   template< typename T >
+   void IntrusivePtr< T >::swap( IntrusivePtr& other ) noexcept
+   {
+      std::swap( m_ptr, other.m_ptr );
+   }
+
+   template< typename T >
+   T* IntrusivePtr< T >::get( ) const noexcept
+   {
+      return m_ptr;
+   }
+
+   template< typename T >
+   T& IntrusivePtr< T >::operator*( ) const
+   {
+      return *m_ptr;
+   }
+
+   template< typename T >
+   T* IntrusivePtr< T >::operator->( ) const noexcept
+   {
+      return m_ptr;
+   }
+
+   template< typename T >
+   IntrusivePtr< T >::operator bool( ) const noexcept
+   {
+      return nullptr != m_ptr;
+   }
+
+   template< typename T >
+   bool IntrusivePtr< T >::operator==( const IntrusivePtr& other ) const noexcept
+   {
+      return m_ptr == other.m_ptr;
+   }
+
+   template< typename T >
+   bool IntrusivePtr< T >::operator!=( const IntrusivePtr& other ) const noexcept
+   {
+      return m_ptr != other.m_ptr;
+   }
+
+   template< typename T >
+   bool IntrusivePtr< T >::operator==( std::nullptr_t ) const noexcept
+   {
+      return nullptr == m_ptr;
+   }
+
+   template< typename T >
+   bool IntrusivePtr< T >::operator!=( std::nullptr_t ) const noexcept
+   {
+      return nullptr != m_ptr;
+   }
 
 
 
